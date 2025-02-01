1696e773a5aa4d5afcb945c59a6f6c2dfb6fefbb
debayudh2001 <152640749+debayudh2001@users.noreply.github.com>
2025-02-02 00:43:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let btn = document.querySelector("input")
+let sel = document.querySelector("#colorSelect")
+let opts = document.querySelectorAll("option")
+btn.addEventListener("click", () => {
+	//console.log("Clicked")
+	for(let v of opts){
+		if(sel.value == v.innerText){
+			sel.removeChild(v)
+			break
+		}
+	}
+})
+
